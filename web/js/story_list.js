5b19063e6067521c2eb1819cdabf435594774143
kimbongju02 <qhdwn25@gmail.com>
2024-07-31 23:22:06 +0900

    load all story list on story_list page

--- web/js/story_list.js
+++ web/js/story_list.js
@@ -1,46 +1,50 @@
 const contentsContainer = document.querySelector('.contents');
-
+var storyList;
 
 window.onload = async function(){
-    request_story_list();
+    request_storyList();
 }
 
-function create_story(story_list, position, idx1, idx2){
-    console.log(story_list);
-    const story_list_idx1 = story_list[idx1];
-    const story_list_idx2 = story_list[idx2];
+function create_story(position, idx1, idx2){
+    const storyListIdx1 = storyList[idx1];
+    const storyListIdx2 = storyList[idx2];
     const result = `<div class="${position}">\n`+
-        `<div class="up-box" id="${story_list_idx1.id}"\n`+
-        common_html(story_list_idx1)+
+        `<div class="up-box" id="${storyListIdx1.id}"\n`+
+        common_html(storyListIdx1)+
         "\n"+
-        `<div class="down-box" id="${story_list_idx2.id}"\n`+
-        common_html(story_list_idx2);
+        `<div class="down-box" id="${storyListIdx2.id}"\n`+
+        common_html(storyListIdx2)+
+        "\n</div>";
     return result;
 }
 
 function common_html(story){
     const result = `style="background-image: url(${story.image})"\n`+
         `onclick="window.location.href = '/detail_story/${story.id}'" style="cursor:pointer;">\n`+
-        `<div class="box-background"></div>\n`+
-        `<div class="box-content">\n`+
+        '<div class="box-background"></div>\n'+
+        '<div class="box-content">\n'+
         `<div class="box-title">${story.title}</div>\n`+
-        `</div>\n`+
-        `</div>`;
+        '</div>\n'+
+        '</div>';
     return result;
 }
 
-function create_story_list(story_list){
-    const create_story_list = create_story(story_list, 'left',1,2);
-    console.log(create_story_list);
-    contentsContainer.innerHTML = create_story_list;
+async function create_storyList(){
+    const contentsClassList = ['left','mid','right'];
+    let storyListHtml = "";
+    for(let i=0; i<3; i++){
+        storyListHtml += create_story(contentsClassList[i], i*2, i*2+1)
+    }
+    console.log(storyListHtml);
+    contentsContainer.innerHTML = storyListHtml;
 }
 
-async function request_story_list(){
+async function request_storyList(){
     fetch('/get_story_list')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        create_story_list(data);
+        storyList = data;
+        create_storyList();
     })
     .catch(error =>{
         console.error('Error:', error);
