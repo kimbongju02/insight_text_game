3f2cfc61523a49b03b7deec8dd59adf075e3f4f8
kimbongju02 <qhdwn25@gmail.com>
2024-07-31 14:55:48 +0900

    load json

--- web/js/test.js
+++
@@ -1,5 +0,0 @@
-import path from 'path';
-const __dirname = path.resolve();
-//const datasetFile_path = path.join(__dirname, '../../dataset', 'novel_data.json');
-const datasetFile_path = '../../dataset/novel_data.json';
-console.log(datasetFile_path);
